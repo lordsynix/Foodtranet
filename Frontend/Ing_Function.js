932ef42e203674a37266ca65179b45a297deb6c7
BabylonMattel <142510094+BabylonMattel@users.noreply.github.com>
2023-09-18 14:59:34 +0200

    Update Ing_Function.js

--- Frontend/Ing_Function.js
+++ Frontend/Ing_Function.js
@@ -1,4 +1,6 @@
 // script.js
+const ingredientsArray = []; // Initialize an array to store ingredients
+
 function addIngredient(event) {
     if (event.key === 'Enter') {
         event.preventDefault(); // Prevent form submission
@@ -10,13 +12,30 @@ function addIngredient(event) {
             listItem.textContent = ingredient;
             ingredientList.appendChild(listItem);
             inputElement.value = ''; // Clear the input field
+
+            // Add the ingredient to the array
+            ingredientsArray.push(ingredient);
+
+            // Automatically save the updated array
+            saveIngredients();
         }
     }
 }
 
 function clearIngredients() {
     const ingredientList = document.getElementById('ingredientList');
-    while (ingredientList.firstChild) {
-        ingredientList.removeChild(ingredientList.firstChild);
-    }
+    ingredientList.innerHTML = ''; // Clear the list
+    ingredientsArray.length = 0; // Clear the array
+
+    // Automatically save the cleared array
+    saveIngredients();
+}
+
+function saveIngredients() {
+    // You can use the ingredientsArray for further processing or storage
+    console.log(ingredientsArray);
+
+    // For example, you can convert it to JSON and send it to a server
+    const ingredientsJSON = JSON.stringify(ingredientsArray);
+    // Then you can send 'ingredientsJSON' to your server or save it to localStorage, etc.
 }
