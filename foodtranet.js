dad805dc684ce7c308d2d549d8d43c3634193162
luisschifferlis <schifferliluis@gmail.com>
2023-09-18 14:50:57 +0200

    ingredients

---
+++ foodtranet.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    function addIngredient(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Prevent form submission
+            const inputElement = document.getElementById('ingredients');
+            const ingredient = inputElement.value.trim();
+            if (ingredient !== '') {
+                const ingredientList = document.getElementById('ingredientList');
+                const listItem = document.createElement('li');
+                listItem.textContent = ingredient;
+                ingredientList.appendChild(listItem);
+                inputElement.value = ''; // Clear the input field
+            }
+        }
+    }
+
+    function clearIngredients() {
+        const ingredientList = document.getElementById('ingredientList');
+        while (ingredientList.firstChild) {
+            ingredientList.removeChild(ingredientList.firstChild);
+        }
+    }
+});
