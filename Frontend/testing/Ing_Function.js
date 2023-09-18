603626a62a65f813ee7752dd3fc2a8f4b35db7ac
BabylonMattel <142510094+BabylonMattel@users.noreply.github.com>
2023-09-18 18:34:41 +0200

    Button style

--- Frontend/testing/Ing_Function.js
+++ Frontend/testing/Ing_Function.js
@@ -1,5 +1,7 @@
 // script.js
-const ingredientsArray = []; // Initialize an array to store ingredients
+
+// Initialize an array to store ingredients
+const ingredientsArray = [];
 
 function addIngredient(event) {
     if (event.key === 'Enter') {
@@ -9,20 +11,25 @@ function addIngredient(event) {
         if (ingredient !== '') {
             const ingredientList = document.getElementById('ingredientList');
             const listItem = document.createElement('li');
-            listItem.textContent = ingredient;
-            ingredientList.appendChild(listItem);
-            inputElement.value = ''; // Clear the input field
 
+            // Create a span for the ingredient text
+            const ingredientSpan = document.createElement('span');
+            ingredientSpan.textContent = ingredient;
+
+            // Create a button to delete the ingredient
             const deleteButton = document.createElement('button');
             deleteButton.textContent = 'Delete';
             deleteButton.onclick = function() {
-                deleteIngredient(ingredient);
+                deleteIngredient(ingredient, listItem);
             };
-            
+            deleteButton.classList.add('delete-button'); // Apply the class
+
+            // Append the span and button to the list item
+            listItem.appendChild(ingredientSpan);
             listItem.appendChild(deleteButton);
+
             ingredientList.appendChild(listItem);
             inputElement.value = ''; // Clear the input field
-
 
             // Add the ingredient to the array
             ingredientsArray.push(ingredient);
@@ -42,6 +49,18 @@ function clearIngredients() {
     saveIngredients();
 }
 
+function deleteIngredient(ingredient, listItem) {
+    const ingredientList = document.getElementById('ingredientList');
+    const index = ingredientsArray.indexOf(ingredient);
+    if (index !== -1) {
+        ingredientsArray.splice(index, 1); // Remove the ingredient from the array
+        ingredientList.removeChild(listItem); // Remove the ingredient from the list
+
+        // Automatically save the updated array
+        saveIngredients();
+    }
+}
+
 function saveIngredients() {
     // You can use the ingredientsArray for further processing or storage
     console.log(ingredientsArray);
@@ -49,19 +68,4 @@ function saveIngredients() {
     // For example, you can convert it to JSON and send it to a server
     const ingredientsJSON = JSON.stringify(ingredientsArray);
     // Then you can send 'ingredientsJSON' to your server or save it to localStorage, etc.
-}
-
-
-
-
-function deleteIngredient(ingredient) {
-    const ingredientList = document.getElementById('ingredientList');
-    const index = ingredientsArray.indexOf(ingredient);
-    if (index !== -1) {
-        ingredientsArray.splice(index, 1); // Remove the ingredient from the array
-        ingredientList.removeChild(ingredientList.childNodes[index]); // Remove the ingredient from the list
-
-        // Automatically save the updated array
-        saveIngredients();
-    }
 }
