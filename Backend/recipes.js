bb4fb3b0ee0e20d50d96a8d5e43ac8805bded38f
lordsynix <leSynix22@gmail.com>
2023-09-21 10:44:40 +0200

    recipes.js updated

--- Backend/recipes.js
+++ Backend/recipes.js
@@ -20,24 +20,7 @@ function initializeRecipes() {
   });
 }
 
-/*function searchRecipes(ingredients) {
-
-  // Filtern Sie die Rezepte, die das erste gegebene Ingredient enthalten
-  const possibleMatches = recipes.forEach(recipe => {
-    recipe.ingredients.includes(ingredients[0]);
-  });
-
-  const matchingRecipes = possibleMatches.forEach(possibleMatch => {
-    recipe.ingredients.includes(ingredients);
-  })
-
-  console.log(matchingRecipes);
-}*/
-
 function searchRecipes(ingredients) {
-
-  // Filtern Sie die Rezepte, die das erste gegebene Ingredient enthalten
-
   console.log(ingredients);
   const possibleMatches = [];
   recipes.forEach(recipe => {
@@ -49,20 +32,8 @@ function searchRecipes(ingredients) {
       });
     }
   });
-  /*const Matches = [];
-  if (possibleMatches.hasOwnProperty('ingredients')) {
-    for (let i = 1; i < ingredients.length; i++) {
-      for (let j = 0; j < possibleMatches.length; j++) {
-      if (possibleMatches[j].includes == ingredients[i]) {
-        Matches.push(possibleMatches[j]);
-      }
-      }
 
-
-    }
-  }
-  */
-  const Matches = [];
+  let Matches = [];
   if (possibleMatches.hasOwnProperty('ingredients')) {
     for (let j = 0; j < possibleMatches.length; j++) {
       for (let i = 1; i < ingredients.length; i++) {
@@ -72,15 +43,12 @@ function searchRecipes(ingredients) {
       }
     }
   }
+  console.log(Matches);
+  console.log(possibleMatches);
+  return Matches[0];
 }
 
-
-          console.log(Matches);
-          console.log(possibleMatches);
-          return Matches[0];
-        
-
-        module.exports = {
-          initializeRecipes,
-          searchRecipes
-        };
+module.exports = {
+  initializeRecipes,
+  searchRecipes
+};
