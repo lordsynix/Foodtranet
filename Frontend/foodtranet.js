8511324d6ba5d24fe804532ca8cf4ceacadc1cad
luisschifferlis <schifferliluis@gmail.com>
2023-09-21 09:48:57 +0200

    progressbar

--- Frontend/foodtranet.js
+++ Frontend/foodtranet.js
@@ -107,3 +107,20 @@ noAllergies.addEventListener('change', function() {
 aktualisiereCheckboxWert(vegan);
 aktualisiereCheckboxWert(vegetarian);
 aktualisiereCheckboxWert(noAllergies);
+
+
+const progressBar = document.querySelector('.bar');
+let progress = 0;
+
+function updateProgressBar() {
+    if (progress < 100) {
+        progress++;
+        progressBar.style.width = `${progress}%`;
+        progressBar.setAttribute('data-progress', progress); // Setzen Sie das Attribut data-progress
+    } else {
+        clearInterval(interval);
+        progressBar.style.display = 'none'
+    }
+}
+
+const interval = setInterval(updateProgressBar, 1000);
