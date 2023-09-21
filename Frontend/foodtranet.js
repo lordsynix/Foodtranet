5e9295c4f5f928d61a44829a6d1194fee5662dec
luisschifferlis <schifferliluis@gmail.com>
2023-09-21 19:15:58 +0200

    code refactoring

--- Frontend/foodtranet.js
+++ Frontend/foodtranet.js
@@ -1,98 +1,4 @@
-const ingredientsArray = []; // Initialize an array to store ingredients
-const searchIngredients = [];
-
-
-function addIngredient(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault(); // Prevent form submission
-        const inputElement = document.getElementById('ingredients');
-        const ingredient = inputElement.value.trim();
-        if (ingredient !== '') {
-            const ingredientList = document.getElementById('ingredientList');
-            const listItem = document.createElement('li');
-            listItem.classList.add('ingredientList')
-
-            // Create a span for the ingredient text
-            const ingredientSpan = document.createElement('span');
-            ingredientSpan.textContent = ingredient;
-
-            // Create a button to delete the ingredient
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'Delete';
-            deleteButton.onclick = function() {
-                deleteIngredient(ingredient, listItem);
-            };
-            deleteButton.classList.add('deleteButton'); // Apply the class
-
-            // Append the span and button to the list item
-            listItem.appendChild(ingredientSpan);
-            listItem.appendChild(deleteButton);
-
-            ingredientList.appendChild(listItem);
-            inputElement.value = ''; // Clear the input field
-
-            // Add the ingredient to the array
-            ingredientsArray.push(ingredient);
-            searchIngredients.push(ingredient);
-
-            console.log(searchIngredients);
-
-            // Automatically save the updated array
-            saveIngredients();
-            
-            
-        }
-    }
-}
-
-function deleteIngredient(ingredient, listItem) {
-    const ingredientList = document.getElementById('ingredientList');
-    const index = ingredientsArray.indexOf(ingredient);
-    if (index !== -1) {
-        ingredientsArray.splice(index, 1); // Remove the ingredient from the array
-        ingredientList.removeChild(listItem); // Remove the ingredient from the list
-
-        // Automatically save the updated array
-        saveIngredients();
-    }
-}
-
-
-function clearIngredients() {
-    const ingredientList = document.getElementById('ingredientList');
-    ingredientList.innerHTML = ''; // Clear the list
-    searchIngredients.length = 0; // Clear the array
-
-    // Automatically save the cleared array
-    saveIngredients();
-}
-
-function saveIngredients() {
-    // You can use the ingredientsArray for further processing or storage
-    
-
-    // For example, you can convert it to JSON and send it to a server
-    const ingredientsJSON = JSON.stringify(ingredientsArray);
-    // Then you can send 'ingredientsJSON' to your server or save it to localStorage, etc.
-}
-
-
-// Eine Funktion erstellen, um den Wert einer Checkbox als Boolean zu speichern und anzuzeigen
-function aktualisiereCheckboxWert(checkboxElement) {
-    const wertAlsBoolean = checkboxElement.checked;
-    console.log(checkboxElement.id, wertAlsBoolean);
-    // Hier kannst du den Wert weiterverarbeiten oder speichern
-}
-
-function searchRecipes() {
-    const params = new URLSearchParams();
-    params.append('ingredients', JSON.stringify(ingredientsArray));
-    
-    const url = `searchRecipes.html?${params.toString()}`;
-
-    window.open(url, '_blank');
-    
-}
+const searchIngredients = []; // Create an emty array
 
 // Die Checkbox-Elementreferenzen abrufen
 const vegan = document.getElementById('vegan');
@@ -112,16 +18,100 @@ noAllergies.addEventListener('change', function() {
     aktualisiereCheckboxWert(noAllergies); 
 });
 
+// Eine Funktion erstellen, um den Wert einer Checkbox als Boolean zu speichern und anzuzeigen
+function aktualisiereCheckboxWert(checkboxElement) {
+    const wertAlsBoolean = checkboxElement.checked;
+    console.log(checkboxElement.id, wertAlsBoolean);
+    // Hier kannst du den Wert weiterverarbeiten oder speichern
+}
 
-// Die Funktion einmalig aufrufen, um die initialen Werte anzuzeigen
-aktualisiereCheckboxWert(vegan);
-aktualisiereCheckboxWert(vegetarian);
-aktualisiereCheckboxWert(noAllergies);
+function searchRecipes() {
+    const params = new URLSearchParams();
+    params.append('ingredients', JSON.stringify(ingredientsArray));
+    
+    const url = `searchRecipes.html?${params.toString()}`;
+
+    window.open(url, '_blank');
+    
+}
+
+function storeInput(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent form submission
+        const inputElement = document.getElementById('ingredientInput');
+        const ingredient = inputElement.value.trim();
+        if (ingredient !== '') {
+            addIngredientToTable(ingredient);
+            addIngredientToArray(ingredient);
+            inputElement.value = '';
+        }
+    }
+}
+
+function addIngredientToTable(ingredient) {
+    // Erstellen Sie eine <tr>-Zeile
+    const row = document.createElement('tr');
+
+    // Erstellen Sie eine <td>-Zelle für das Element
+    const cell1 = document.createElement('td');
+    cell1.textContent = ingredient;
+
+    // Erstellen Sie eine <td>-Zelle für den Button
+    const cell2 = document.createElement('td');
+    const button = document.createElement('button');
+    button.textContent = 'Delete'; 
+    button.addEventListener('click', function() {
+        deleteIngredient(ingredient); 
+    });
+    cell2.appendChild(button);
+
+    row.setAttribute('id', ingredient)
+    row.classList.add('ingredientRow');
+
+    // Add cells to the rows
+    row.appendChild(cell1);
+    row.appendChild(cell2);
+
+    // Add the row to the table
+    document.getElementById('ingredientTable').appendChild(row);
+}
+
+function updateArray() {
+    console.log(searchIngredients);
+}
+
+function addIngredientToArray(ingredient) {
+    searchIngredients.push(ingredient);
+    updateArray();
+}
+
+function deleteIngredient(ingredient) {
+    var rowId = document.getElementById(ingredient);
+    var i = rowId.parentNode.parentNode.parentNode.rowIndex;
+    document.getElementById('ingredientTable').deleteRow(i)
+    searchIngredients.splice(searchIngredients.indexOf(ingredient), 1)
+    updateArray();
+}
+
+function clearIngredients() {
+    // Holen Sie sich die Tabelle, die Sie löschen möchten, z.B. mit einer ID
+    const table = document.getElementById('ingredientTable'); // Ersetzen Sie 'meineTabelle' durch die tatsächliche ID Ihrer Tabelle
+
+    if (table) {
+        // Durchlaufen Sie die Zeilen der Tabelle von unten nach oben und löschen Sie sie
+        for (let i = table.rows.length - 1; i >= 0; i--) {
+            table.deleteRow(i);
+        }
+    }
+
+    searchIngredients.length = 0; // Clear the array
+    updateArray();
+}
 
 fetch('http://localhost:3000/api/getIngredients')
     .then(response => response.json())
     .then(data => {
-        const inputField = document.getElementById('ingredients');
+        const inputField = document.getElementById('ingredientInput');
         const list = document.getElementById('autocomplete-list');
         
         inputField.addEventListener('input', function() {
@@ -144,57 +134,13 @@ fetch('http://localhost:3000/api/getIngredients')
 
             filteredData.forEach(item => {
                 const listItem = document.createElement('li');
-                const AutolistItem = document.createElement('li');
                 listItem.textContent = item;
                 
                 listItem.addEventListener('click', function() {
-                    
-                    searchIngredients.push(filteredData[0]);
-                    console.log(searchIngredients);
-                    
-                    const inputElement = document.getElementById('ingredients');
-                    const ingredient = inputElement.value.trim();
-                    if (ingredient !== '') {
-                        
-                        const ingredientList = document.getElementById('ingredientList');
-                        const listItem = document.createElement('li');
-                        listItem.classList.add('ingredientList')
-
-                        // Create a span for the ingredient text
-                        const ingredientSpan = document.createElement('span');
-                        ingredientSpan.textContent = ingredient;
-
-                        // Create a button to delete the ingredient
-                        const deleteButton = document.createElement('button');
-                        deleteButton.textContent = 'Delete';
-                        deleteButton.onclick = function () {
-                            listItem.removeChild(AutolistItem);
-                            listItem.removeChild(deleteButton)
-                        };
-                        deleteButton.classList.add('deleteButton'); // Apply the class
-
-                        
-                        
-
-                        // Append the span and button to the list item
-                        inputElement.value = ''; // Clear the input field
-                        
-
-                        ingredientList.appendChild(listItem);
-                        AutolistItem.textContent = item;
-                        listItem.appendChild(AutolistItem);
-                        AutolistItem.appendChild(deleteButton);
-
-                        
-                        
-                                        
-                        
-
-
-                        // Automatically save the updated array
-                        saveIngredients();
-                    }
-                    
+                    addIngredientToTable(filteredData[0]);
+                    addIngredientToArray(filteredData[0]);
+                    const inputElement = document.getElementById('ingredientInput');
+                    inputElement.value = '';
                 });
                 list.appendChild(listItem);
             });
@@ -212,6 +158,3 @@ fetch('http://localhost:3000/api/getIngredients')
     .catch(error => {
         console.error('Error loading JSON data:', error);
     });
-
-    
-
